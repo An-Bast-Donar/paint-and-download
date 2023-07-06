cacea6cca40cfe0ccb703fa06e4934ecd68a6988
An Bast <sebastianrondonja@hotmail.com>
2023-07-05 19:20:52 -0500

    inicio

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
 canvas.width = 512;
 canvas.height = 512;
-
 // Aquí es donde puedes empezar a dibujar tu imagen utilizando métodos de canvas
 
+
+
 // Descargar la imagen
-
 const downloadBtn = document.getElementById("download-btn");
-
 downloadBtn.addEventListener("click", function () {
   const link = document.createElement("a");
   link.download = "mi-imagen.png";
